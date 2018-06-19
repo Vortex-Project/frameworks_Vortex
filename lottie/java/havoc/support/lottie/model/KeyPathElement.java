BootleggersROM/frameworks_bootleggers

Import Lottie by Airbnb
A simple library for loading json based animated drawables
https://github.com/airbnb/lottie-android

Current version: 2.5.4

lottie/java/aoscp/support/lottie/model/KeyPathElement.java
@@ -1,0 +1,37 @@
+package aoscp.support.lottie.model;
+
+import android.support.annotation.Nullable;
+
+import aoscp.support.lottie.value.LottieValueCallback;
+
+import java.util.List;
+
+/**
+ * Any item that can be a part of a {@link KeyPath} should implement this.
+ */
+public interface KeyPathElement {
+
+  /**
+   * Called recursively during keypath resolution.
+   *
+   * The overridden method should just call:
+   *        MiscUtils.resolveKeyPath(keyPath, depth, accumulator, currentPartialKeyPath, this);
+   *
+   * @param keyPath The full keypath being resolved.
+   * @param depth The current depth that this element should be checked at in the keypath.
+   * @param accumulator A list of fully resolved keypaths. If this element fully matches the
+   *                    keypath then it should add itself to this list.
+   * @param currentPartialKeyPath A keypath that contains all parent element of this one.
+   *                              This element should create a copy of this and append itself
+   *                              with KeyPath#addKey when it adds itself to the accumulator
+   *                              or propagates resolution to its children.
+   */
+  void resolveKeyPath(
+      KeyPath keyPath, int depth, List<KeyPath> accumulator, KeyPath currentPartialKeyPath);
+
+  /**
+   * The overridden method should handle appropriate properties and set value callbacks on their
+   * animations.
+   */
+  <T> void addValueCallback(T property, @Nullable LottieValueCallback<T> callback);
+}

lottie/java/aoscp/support/lottie/model/animatable/BaseAnimatableValue.java
@@ -1,0 +1,30 @@
+package aoscp.support.lottie.model.animatable;
+
+import aoscp.support.lottie.value.Keyframe;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+abstract class BaseAnimatableValue<V, O> implements AnimatableValue<V, O> {
+  final List<Keyframe<V>> keyframes;
+
+  /**
+   * Create a default static animatable path.
+   */
+  BaseAnimatableValue(V value) {
+    this(Collections.singletonList(new Keyframe<>(value)));
+  }
+
+  BaseAnimatableValue(List<Keyframe<V>> keyframes) {
+    this.keyframes = keyframes;
+  }
+
+  @Override public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    if (!keyframes.isEmpty()) {
+      sb.append("values=").append(Arrays.toString(keyframes.toArray()));
+    }
+    return sb.toString();
+  }
+}

lottie/java/aoscp/support/lottie/parser/ColorParser.java
@@ -1,0 +1,36 @@
+package aoscp.support.lottie.parser;
+
+import android.graphics.Color;
+import android.util.JsonReader;
+import android.util.JsonToken;
+
+import java.io.IOException;
+
+public class ColorParser implements ValueParser<Integer> {
+  public static final ColorParser INSTANCE = new ColorParser();
+
+  private ColorParser() {}
+
+  @Override public Integer parse(JsonReader reader, float scale) throws IOException {
+    boolean isArray = reader.peek() == JsonToken.BEGIN_ARRAY;
+    if (isArray) {
+      reader.beginArray();
+    }
+    double r = reader.nextDouble();
+    double g = reader.nextDouble();
+    double b = reader.nextDouble();
+    double a = reader.nextDouble();
+    if (isArray) {
+      reader.endArray();
+    }
+
+    if (r <= 1 && g <= 1 && b <= 1 && a <= 1) {
+      r *= 255;
+      g *= 255;
+      b *= 255;
+      a *= 255;
+    }
+
+    return Color.argb((int) a, (int) r, (int) g, (int) b);
+  }
+}

lottie/java/aoscp/support/lottie/parser/GradientColorParser.java
@@ -1,0 +1,145 @@
+package aoscp.support.lottie.parser;
+
+import android.graphics.Color;
+import android.support.annotation.IntRange;
+import android.util.JsonReader;
+import android.util.JsonToken;
+
+import aoscp.support.lottie.model.content.GradientColor;
+import aoscp.support.lottie.utils.MiscUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradientColorParser implements aoscp.support.lottie.parser.ValueParser<GradientColor> {
+  /** The number of colors if it exists in the json or -1 if it doesn't (legacy bodymovin) */
+  private int colorPoints;
+
+  public GradientColorParser(int colorPoints) {
+    this.colorPoints = colorPoints;
+  }
+
+  /**
+   * Both the color stops and opacity stops are in the same array.
+   * There are {@link #colorPoints} colors sequentially as:
+   * [
+   *     ...,
+   *     position,
+   *     red,
+   *     green,
+   *     blue,
+   *     ...
+   * ]
+   *
+   * The remainder of the array is the opacity stops sequentially as:
+   * [
+   *     ...,
+   *     position,
+   *     opacity,
+   *     ...
+   * ]
+   */
+  @Override public GradientColor parse(JsonReader reader, float scale)
+      throws IOException {
+    List<Float> array = new ArrayList<>();
+    // The array was started by Keyframe because it thought that this may be an array of keyframes
+    // but peek returned a number so it considered it a static array of numbers.
+    boolean isArray = reader.peek() == JsonToken.BEGIN_ARRAY;
+    if (isArray) {
+      reader.beginArray();
+    }
+    while (reader.hasNext()) {
+      array.add((float) reader.nextDouble());
+    }
+    if(isArray) {
+      reader.endArray();
+    }
+    if (colorPoints == -1) {
+      colorPoints = array.size() / 4;
+    }
+
+    float[] positions = new float[colorPoints];
+    int[] colors = new int[colorPoints];
+
+    int r = 0;
+    int g = 0;
+    for (int i = 0; i < colorPoints * 4; i++) {
+      int colorIndex = i / 4;
+      double value = array.get(i);
+      switch (i % 4) {
+        case 0:
+          // position
+          positions[colorIndex] = (float) value;
+          break;
+        case 1:
+          r = (int) (value * 255);
+          break;
+        case 2:
+          g = (int) (value * 255);
+          break;
+        case 3:
+          int b = (int) (value * 255);
+          colors[colorIndex] = Color.argb(255, r, g, b);
+          break;
+      }
+    }
+
+    GradientColor gradientColor = new GradientColor(positions, colors);
+    addOpacityStopsToGradientIfNeeded(gradientColor, array);
+    return gradientColor;
+  }
+
+  /**
+   * This cheats a little bit.
+   * Opacity stops can be at arbitrary intervals independent of color stops.
+   * This uses the existing color stops and modifies the opacity at each existing color stop
+   * based on what the opacity would be.
+   *
+   * This should be a good approximation is nearly all cases. However, if there are many more
+   * opacity stops than color stops, information will be lost.
+   */
+  private void addOpacityStopsToGradientIfNeeded(GradientColor gradientColor, List<Float> array) {
+    int startIndex = colorPoints * 4;
+    if (array.size() <= startIndex) {
+      return;
+    }
+
+    int opacityStops = (array.size() - startIndex) / 2;
+    double[] positions = new double[opacityStops];
+    double[] opacities = new double[opacityStops];
+
+    for (int i = startIndex, j = 0; i < array.size(); i++) {
+      if (i % 2 == 0) {
+        positions[j] = array.get(i);
+      } else {
+        opacities[j] = array.get(i);
+        j++;
+      }
+    }
+
+    for (int i = 0; i < gradientColor.getSize(); i++) {
+      int color = gradientColor.getColors()[i];
+      color = Color.argb(
+          getOpacityAtPosition(gradientColor.getPositions()[i], positions, opacities),
+          Color.red(color),
+          Color.green(color),
+          Color.blue(color)
+      );
+      gradientColor.getColors()[i] = color;
+    }
+  }
+
+  @IntRange(from=0, to=255)
+  private int getOpacityAtPosition(double position, double[] positions, double[] opacities) {
+    for (int i = 1; i < positions.length; i++) {
+      double lastPosition = positions[i - 1];
+      double thisPosition = positions[i];
+      if (positions[i] >= position) {
+        double progress = (position - lastPosition) / (thisPosition - lastPosition);
+        return (int) (255 * MiscUtils.lerp(opacities[i - 1], opacities[i], progress));
+      }
+    }
+    return (int) (255 * opacities[opacities.length - 1]);
+  }
+}

lottie/java/aoscp/support/lottie/value/LottieInterpolatedFloatValue.java
@@ -1,0 +1,21 @@
+package aoscp.support.lottie.value;
+
+import android.view.animation.Interpolator;
+
+import aoscp.support.lottie.utils.MiscUtils;
+
+@SuppressWarnings("unused")
+public class LottieInterpolatedFloatValue extends LottieInterpolatedValue<Float> {
+
+  public LottieInterpolatedFloatValue(Float startValue, Float endValue) {
+    super(startValue, endValue);
+  }
+
+  public LottieInterpolatedFloatValue(Float startValue, Float endValue, Interpolator interpolator) {
+    super(startValue, endValue, interpolator);
+  }
+
+  @Override Float interpolateValue(Float startValue, Float endValue, float progress) {
+    return MiscUtils.lerp(startValue, endValue, progress);
+  }
+}

lottie/java/aoscp/support/lottie/value/LottieRelativeIntegerValueCallback.java
@@ -1,0 +1,32 @@
+package aoscp.support.lottie.value;
+
+import aoscp.support.lottie.utils.MiscUtils;
+
+/**
+ * {@link LottieValueCallback} that provides a value offset from the original animation
+ * rather than an absolute value.
+ */
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class LottieRelativeIntegerValueCallback extends LottieValueCallback<Integer> {
+  @Override
+  public Integer getValue(LottieFrameInfo<Integer> frameInfo) {
+    int originalValue = MiscUtils.lerp(
+        frameInfo.getStartValue(),
+        frameInfo.getEndValue(),
+        frameInfo.getInterpolatedKeyframeProgress()
+    );
+    int newValue = getOffset(frameInfo);
+    return originalValue + newValue;
+  }
+
+  /**
+   * Override this to provide your own offset on every frame.
+   */
+  public Integer getOffset(LottieFrameInfo<Integer> frameInfo) {
+    if (value == null) {
+      throw new IllegalArgumentException("You must provide a static value in the constructor " +
+          ", call setValue, or override getValue.");
+    }
+    return value;
+  }
+}

Vortex-Project/frameworks_Vortex

lottie/java/havoc/support/lottie/model/KeyPathElement.java
@@ -1,0 +1,37 @@
+package havoc.support.lottie.model;
+
+import android.support.annotation.Nullable;
+
+import havoc.support.lottie.value.LottieValueCallback;
+
+import java.util.List;
+
+/**
+ * Any item that can be a part of a {@link KeyPath} should implement this.
+ */
+public interface KeyPathElement {
+
+  /**
+   * Called recursively during keypath resolution.
+   *
+   * The overridden method should just call:
+   *        MiscUtils.resolveKeyPath(keyPath, depth, accumulator, currentPartialKeyPath, this);
+   *
+   * @param keyPath The full keypath being resolved.
+   * @param depth The current depth that this element should be checked at in the keypath.
+   * @param accumulator A list of fully resolved keypaths. If this element fully matches the
+   *                    keypath then it should add itself to this list.
+   * @param currentPartialKeyPath A keypath that contains all parent element of this one.
+   *                              This element should create a copy of this and append itself
+   *                              with KeyPath#addKey when it adds itself to the accumulator
+   *                              or propagates resolution to its children.
+   */
+  void resolveKeyPath(
+      KeyPath keyPath, int depth, List<KeyPath> accumulator, KeyPath currentPartialKeyPath);
+
+  /**
+   * The overridden method should handle appropriate properties and set value callbacks on their
+   * animations.
+   */
+  <T> void addValueCallback(T property, @Nullable LottieValueCallback<T> callback);
+}

lottie/java/havoc/support/lottie/model/animatable/BaseAnimatableValue.java
@@ -1,0 +1,30 @@
+package havoc.support.lottie.model.animatable;
+
+import havoc.support.lottie.value.Keyframe;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+abstract class BaseAnimatableValue<V, O> implements AnimatableValue<V, O> {
+  final List<Keyframe<V>> keyframes;
+
+  /**
+   * Create a default static animatable path.
+   */
+  BaseAnimatableValue(V value) {
+    this(Collections.singletonList(new Keyframe<>(value)));
+  }
+
+  BaseAnimatableValue(List<Keyframe<V>> keyframes) {
+    this.keyframes = keyframes;
+  }
+
+  @Override public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    if (!keyframes.isEmpty()) {
+      sb.append("values=").append(Arrays.toString(keyframes.toArray()));
+    }
+    return sb.toString();
+  }
+}

lottie/java/havoc/support/lottie/parser/ColorParser.java
@@ -1,0 +1,36 @@
+package havoc.support.lottie.parser;
+
+import android.graphics.Color;
+import android.util.JsonReader;
+import android.util.JsonToken;
+
+import java.io.IOException;
+
+public class ColorParser implements ValueParser<Integer> {
+  public static final ColorParser INSTANCE = new ColorParser();
+
+  private ColorParser() {}
+
+  @Override public Integer parse(JsonReader reader, float scale) throws IOException {
+    boolean isArray = reader.peek() == JsonToken.BEGIN_ARRAY;
+    if (isArray) {
+      reader.beginArray();
+    }
+    double r = reader.nextDouble();
+    double g = reader.nextDouble();
+    double b = reader.nextDouble();
+    double a = reader.nextDouble();
+    if (isArray) {
+      reader.endArray();
+    }
+
+    if (r <= 1 && g <= 1 && b <= 1 && a <= 1) {
+      r *= 255;
+      g *= 255;
+      b *= 255;
+      a *= 255;
+    }
+
+    return Color.argb((int) a, (int) r, (int) g, (int) b);
+  }
+}

lottie/java/havoc/support/lottie/parser/GradientColorParser.java
@@ -1,0 +1,145 @@
+package havoc.support.lottie.parser;
+
+import android.graphics.Color;
+import android.support.annotation.IntRange;
+import android.util.JsonReader;
+import android.util.JsonToken;
+
+import havoc.support.lottie.model.content.GradientColor;
+import havoc.support.lottie.utils.MiscUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradientColorParser implements havoc.support.lottie.parser.ValueParser<GradientColor> {
+  /** The number of colors if it exists in the json or -1 if it doesn't (legacy bodymovin) */
+  private int colorPoints;
+
+  public GradientColorParser(int colorPoints) {
+    this.colorPoints = colorPoints;
+  }
+
+  /**
+   * Both the color stops and opacity stops are in the same array.
+   * There are {@link #colorPoints} colors sequentially as:
+   * [
+   *     ...,
+   *     position,
+   *     red,
+   *     green,
+   *     blue,
+   *     ...
+   * ]
+   *
+   * The remainder of the array is the opacity stops sequentially as:
+   * [
+   *     ...,
+   *     position,
+   *     opacity,
+   *     ...
+   * ]
+   */
+  @Override public GradientColor parse(JsonReader reader, float scale)
+      throws IOException {
+    List<Float> array = new ArrayList<>();
+    // The array was started by Keyframe because it thought that this may be an array of keyframes
+    // but peek returned a number so it considered it a static array of numbers.
+    boolean isArray = reader.peek() == JsonToken.BEGIN_ARRAY;
+    if (isArray) {
+      reader.beginArray();
+    }
+    while (reader.hasNext()) {
+      array.add((float) reader.nextDouble());
+    }
+    if(isArray) {
+      reader.endArray();
+    }
+    if (colorPoints == -1) {
+      colorPoints = array.size() / 4;
+    }
+
+    float[] positions = new float[colorPoints];
+    int[] colors = new int[colorPoints];
+
+    int r = 0;
+    int g = 0;
+    for (int i = 0; i < colorPoints * 4; i++) {
+      int colorIndex = i / 4;
+      double value = array.get(i);
+      switch (i % 4) {
+        case 0:
+          // position
+          positions[colorIndex] = (float) value;
+          break;
+        case 1:
+          r = (int) (value * 255);
+          break;
+        case 2:
+          g = (int) (value * 255);
+          break;
+        case 3:
+          int b = (int) (value * 255);
+          colors[colorIndex] = Color.argb(255, r, g, b);
+          break;
+      }
+    }
+
+    GradientColor gradientColor = new GradientColor(positions, colors);
+    addOpacityStopsToGradientIfNeeded(gradientColor, array);
+    return gradientColor;
+  }
+
+  /**
+   * This cheats a little bit.
+   * Opacity stops can be at arbitrary intervals independent of color stops.
+   * This uses the existing color stops and modifies the opacity at each existing color stop
+   * based on what the opacity would be.
+   *
+   * This should be a good approximation is nearly all cases. However, if there are many more
+   * opacity stops than color stops, information will be lost.
+   */
+  private void addOpacityStopsToGradientIfNeeded(GradientColor gradientColor, List<Float> array) {
+    int startIndex = colorPoints * 4;
+    if (array.size() <= startIndex) {
+      return;
+    }
+
+    int opacityStops = (array.size() - startIndex) / 2;
+    double[] positions = new double[opacityStops];
+    double[] opacities = new double[opacityStops];
+
+    for (int i = startIndex, j = 0; i < array.size(); i++) {
+      if (i % 2 == 0) {
+        positions[j] = array.get(i);
+      } else {
+        opacities[j] = array.get(i);
+        j++;
+      }
+    }
+
+    for (int i = 0; i < gradientColor.getSize(); i++) {
+      int color = gradientColor.getColors()[i];
+      color = Color.argb(
+          getOpacityAtPosition(gradientColor.getPositions()[i], positions, opacities),
+          Color.red(color),
+          Color.green(color),
+          Color.blue(color)
+      );
+      gradientColor.getColors()[i] = color;
+    }
+  }
+
+  @IntRange(from=0, to=255)
+  private int getOpacityAtPosition(double position, double[] positions, double[] opacities) {
+    for (int i = 1; i < positions.length; i++) {
+      double lastPosition = positions[i - 1];
+      double thisPosition = positions[i];
+      if (positions[i] >= position) {
+        double progress = (position - lastPosition) / (thisPosition - lastPosition);
+        return (int) (255 * MiscUtils.lerp(opacities[i - 1], opacities[i], progress));
+      }
+    }
+    return (int) (255 * opacities[opacities.length - 1]);
+  }
+}

lottie/java/havoc/support/lottie/value/LottieInterpolatedFloatValue.java
@@ -1,0 +1,21 @@
+package havoc.support.lottie.value;
+
+import android.view.animation.Interpolator;
+
+import havoc.support.lottie.utils.MiscUtils;
+
+@SuppressWarnings("unused")
+public class LottieInterpolatedFloatValue extends LottieInterpolatedValue<Float> {
+
+  public LottieInterpolatedFloatValue(Float startValue, Float endValue) {
+    super(startValue, endValue);
+  }
+
+  public LottieInterpolatedFloatValue(Float startValue, Float endValue, Interpolator interpolator) {
+    super(startValue, endValue, interpolator);
+  }
+
+  @Override Float interpolateValue(Float startValue, Float endValue, float progress) {
+    return MiscUtils.lerp(startValue, endValue, progress);
+  }
+}

lottie/java/havoc/support/lottie/value/LottieRelativeIntegerValueCallback.java
@@ -1,0 +1,32 @@
+package havoc.support.lottie.value;
+
+import havoc.support.lottie.utils.MiscUtils;
+
+/**
+ * {@link LottieValueCallback} that provides a value offset from the original animation
+ * rather than an absolute value.
+ */
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class LottieRelativeIntegerValueCallback extends LottieValueCallback<Integer> {
+  @Override
+  public Integer getValue(LottieFrameInfo<Integer> frameInfo) {
+    int originalValue = MiscUtils.lerp(
+        frameInfo.getStartValue(),
+        frameInfo.getEndValue(),
+        frameInfo.getInterpolatedKeyframeProgress()
+    );
+    int newValue = getOffset(frameInfo);
+    return originalValue + newValue;
+  }
+
+  /**
+   * Override this to provide your own offset on every frame.
+   */
+  public Integer getOffset(LottieFrameInfo<Integer> frameInfo) {
+    if (value == null) {
+      throw new IllegalArgumentException("You must provide a static value in the constructor " +
+          ", call setValue, or override getValue.");
+    }
+    return value;
+  }
+}